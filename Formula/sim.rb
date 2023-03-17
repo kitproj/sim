988ee13401e888d67fcb7d47d2e27baeb65bf349
Alex Collins <alex_collins@intuit.com>
2023-03-17 20:27:05 +0000

    Brew formula update for sim version v0.0.17

--- Formula/sim.rb
+++ Formula/sim.rb
@@ -5,20 +5,20 @@
 class Sim < Formula
   desc "Straight-forward, fast, scalable API simulation."
   homepage "https://github.com/kitproj/sim"
-  version "0.0.16"
+  version "0.0.17"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.16/sim_0.0.16_Darwin_x86_64.tar.gz"
-      sha256 "88db7372b5a7730646b97c4b43bdf221fab3697663968269e93ed14f12b84f5b"
+    if Hardware::CPU.arm?
+      url "https://github.com/kitproj/sim/releases/download/v0.0.17/sim_0.0.17_Darwin_arm64.tar.gz"
+      sha256 "5c4cc32673428f6e926bb07c1dd6a8ea771e6748dc0a5a1a629b6143dcc7dad4"
 
       def install
         bin.install "sim"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.16/sim_0.0.16_Darwin_arm64.tar.gz"
-      sha256 "292df2a237234d77c0b93b3dcb1e4b164f824efc6d0151793d1f4fb88256a621"
+    if Hardware::CPU.intel?
+      url "https://github.com/kitproj/sim/releases/download/v0.0.17/sim_0.0.17_Darwin_x86_64.tar.gz"
+      sha256 "de74e1f3fed47da412f5c2390fb293fe297ae5742b918bbfe9273a7313973299"
 
       def install
         bin.install "sim"
@@ -27,17 +27,17 @@ class Sim < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.16/sim_0.0.16_Linux_arm64.tar.gz"
-      sha256 "f7e703857ecc62ae54c8a51bbd355257f630ec684324a7cad6471ac3ec01cfa6"
+    if Hardware::CPU.intel?
+      url "https://github.com/kitproj/sim/releases/download/v0.0.17/sim_0.0.17_Linux_x86_64.tar.gz"
+      sha256 "e71c0962153c0ca3175e430c42c9a24f12c6157a6ee00aa6f39a51ba105c9bda"
 
       def install
         bin.install "sim"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.16/sim_0.0.16_Linux_x86_64.tar.gz"
-      sha256 "79f3e9d9bc1b07d6f354c08e2fdf2564448219de816825e028563fe23b8d8177"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kitproj/sim/releases/download/v0.0.17/sim_0.0.17_Linux_arm64.tar.gz"
+      sha256 "514b0770af1183765a0e6ae7981ba7712330fd74db5b8b4510825d7e4aea2a56"
 
       def install
         bin.install "sim"
