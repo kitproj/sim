aa0f140c4830ae596cb51e92066746ec08d5f590
Alex Collins <alex_collins@intuit.com>
2023-03-26 09:57:34 -0700

    fix: more robust install

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 set -eux
 
-tag=$(curl --retry 3 -s "https://api.github.com/repos/kitproj/sim/releases/latest" | jq -r '.tag_name')
+tag=
+while [ "$tag" = "" ]; do
+  tag=$(curl -fsL "https://api.github.com/repos/kitproj/sim/releases/latest" | jq -r '.tag_name')
+done
+
 version=$(echo $tag | cut -c 2-)
-url="https://github.com/kitproj/sim/releases/download/${tag}/sim_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
-curl --retry 3 -L $url | tar -zxvf - sim
+url="https://github.com/kitproj/sim/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
+
+while [ ! -e kit ]; do
+  curl -fsL $url | tar -zxvf - sim
+done
+
 chmod +x sim
-sudo mv sim /usr/local/bin/sim
+sudo mv kit /usr/local/bin/sim
