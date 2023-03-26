53b1ddf05f7ae2ee77495ab77b34d5c58da56cf8
Alex Collins <alex_collins@intuit.com>
2023-03-26 17:46:31 +0000

    Brew formula update for sim version v0.0.19

--- Formula/sim.rb
+++ Formula/sim.rb
@@ -5,20 +5,20 @@
 class Sim < Formula
   desc "Straight-forward, fast, scalable API simulation."
   homepage "https://github.com/kitproj/sim"
-  version "0.0.18"
+  version "0.0.19"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.18/sim_0.0.18_Darwin_x86_64.tar.gz"
-      sha256 "e380dc8ce61c7a99538c41ee6882868c88ecb9133615a64b6f8f84c664e75237"
+      url "https://github.com/kitproj/sim/releases/download/v0.0.19/sim_0.0.19_Darwin_x86_64.tar.gz"
+      sha256 "971098973efd1c0a8c884e2d912e8aad7280cdf59ebbcaa5127bee61ab3e6002"
 
       def install
         bin.install "sim"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.18/sim_0.0.18_Darwin_arm64.tar.gz"
-      sha256 "ef67e3162c08ea12aada9185e2e306584f236e91d58836e4ac20169246670181"
+      url "https://github.com/kitproj/sim/releases/download/v0.0.19/sim_0.0.19_Darwin_arm64.tar.gz"
+      sha256 "0391e62be1f3b1b5bea878fd7297890b715bdf034e0fee291d4322f841b2878c"
 
       def install
         bin.install "sim"
@@ -28,16 +28,16 @@ class Sim < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.18/sim_0.0.18_Linux_arm64.tar.gz"
-      sha256 "ed40ebee4afe91c124cc4230cfe0d8e1ccb124cd76d5a3cbcfe3e2fd38f5adf5"
+      url "https://github.com/kitproj/sim/releases/download/v0.0.19/sim_0.0.19_Linux_arm64.tar.gz"
+      sha256 "2ec028b9d860bb73e94a53563943866ecfe63c0b60f164f407771333de1679e9"
 
       def install
         bin.install "sim"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kitproj/sim/releases/download/v0.0.18/sim_0.0.18_Linux_x86_64.tar.gz"
-      sha256 "87687324e59fbe06328fc7b897db5153104b152e68b4ccb8537a82126da48f37"
+      url "https://github.com/kitproj/sim/releases/download/v0.0.19/sim_0.0.19_Linux_x86_64.tar.gz"
+      sha256 "99d384048df914f75d650f71e6ecbd7d07138e6439481c8d1ed4dea93c21a79a"
 
       def install
         bin.install "sim"
